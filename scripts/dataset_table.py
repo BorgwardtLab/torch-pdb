94b39ca75d4874624931f0b77a6c12753aab44dc
Carlos G. Oliver <carlos.gonzalez.oliver@gmail.com>
2022-07-15 13:42:05 +0200

    protein count ready

--- scripts/dataset_table.py
+++ scripts/dataset_table.py
@@ -18,37 +18,28 @@ datasets = [
 
 rows = []
 
-# unique_pdbs = set()
-# for i, dataset in enumerate(datasets):
-    # with tempfile.TemporaryDirectory() as tmp:
-        # ds = dataset(root=tmp)
-        # unique_pdbs |= {p['ID'] for p in ds.proteins}
-        # desc = ds.describe()
-        # rows.append(desc)
+unique_pdbs = set()
+for i, dataset in enumerate(datasets):
+    with tempfile.TemporaryDirectory() as tmp:
+        ds = dataset(root=tmp)
+        unique_pdbs |= {p['ID'] for p in ds.proteins}
+        desc = ds.describe()
+        rows.append(desc)
 
-# print(f"TOTAL PDB : {len(unique_pdbs)}")
 unique_af = set()
 # do alphafold seaprately
 for org in AF_DATASET_NAMES.keys():
-    print(org)
-    if org == 'swissprot':
-        continue
     with tempfile.TemporaryDirectory() as tmp:
         ds = AlphaFoldDataset(root=tmp, organism=org)
         unique_af |= {p['ID'] for p in ds.proteins}
-        print(list(unique_af)[0])
         desc = ds.describe()
         desc['name'] += f'_{org}'
-        print(desc)
         rows.append(desc)
     df = pd.DataFrame(rows)
     md = df.to_markdown(index=False)
     tx = df.to_latex(index=False, na_rep='-')
-    print(md)
-    print()
-    print(tx)
-    break
 
+print(f"TOTAL RCSB: {len(unique_pdbs)}")
 print(f"TOTAL AF: {len(unique_af)}")
 
 df = pd.DataFrame(rows)
